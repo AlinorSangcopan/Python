17867dc71498833b1fc44737bbf8a6a8f29bd546
AlinorSangcopan <sangcopan.alinor321@gmail.com>
2026-02-06 15:33:27 +0800

    Add files via upload

---
+++ week2activity.py
@@ -0,0 +1,16 @@
+fullName = "Alinor A. Sangcopan"
+age = 19
+height = 1.89
+isWorking = False
+
+print("\nFullname: ", fullName)
+print(type(fullName))
+
+print("\nAge: ", age)
+print(type(age))
+
+print("\nHeight in meters: ", height)
+print(type(height))
+
+print("\nCurrently working? ", isWorking)
+print(type(isWorking))
